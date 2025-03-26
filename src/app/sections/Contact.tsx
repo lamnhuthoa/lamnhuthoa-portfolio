bce6b1da5d3a5e84479061a445468190118cabd4
lamnhuthoa <hoalamnhut@gmail.com>
2025-03-26 23:38:57 +0700

    Updated Contact component

--- src/app/sections/Contact.tsx
+++ src/app/sections/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
         <section className={`py-16 text-center ${styles.container}`}>
             <h2 className={`text-3xl font-bold text-primary ${styles.title}`}>Get In Touch</h2>
             <div className={`flex flex-col mt-4 ${styles.contactMeContainer}`}>
-                <p>I'm passionate about turning ideas into reality. Let's connect!</p>
+                <p>{"I'm passionate about turning ideas into reality. Let's connect!"}</p>
                 <a href="mailto:hoalamnhut@gmail.com">
                     <button className={`bg-emerald-green text-white py-2 px-6 mt-4 ${styles['email-me-button']}`}>
                         Send me an email
